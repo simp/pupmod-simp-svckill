d035dbb8d58af3808365850704b78c646a844693
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-06-21 17:07:16 -0400

    (SIMP-1167) Do not include svckill in ignore
    Calling 'svckill::ignore' should not activate 'svckill' by default. This
    is meant to be a safety mechanism and including 'svckill' is definitely
    not safe on a system that isn't expecting it.
    
    SIMP-1167 #comment Found issue during testing
    SIMP-1174 #close

--- spec/defines/ignore_spec.rb
+++ spec/defines/ignore_spec.rb
@@ -6,4 +6,6 @@ describe 'svckill::ignore' do
 
   it { is_expected.to compile.with_all_deps }
   it { is_expected.to create_concat_fragment("svckill_ignore+#{title}.ignore").with_content("#{title}\n") }
+  it { is_expected.to create_concat_build('svckill_ignore') }
+  it { is_expected.to_not create_svckill('svckill') }
 end
