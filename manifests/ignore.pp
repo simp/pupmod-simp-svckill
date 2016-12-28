8690276df11c7167192ee4b02aab683173b72b44
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2016-12-28 11:42:00 -0500

    (SIMP-1695) Updated to use puppetlabs-concat (#19)
    * (SIMP-1695) Updated to use puppetlabs-concat
    
    In the quest to simplify and add all breaking changes
    
    SIMP-2202 #close

--- manifests/ignore.pp
+++ manifests/ignore.pp
@@ -1,11 +1,15 @@
-# Ensure that service $name will not be killed by svckill.
+# Ensure that service ``$name`` will not be killed by svckill
 #
-# @attr name The name of the service to prevent from being killed.
+# @param name [String]
+#   The name of the service to prevent being killed
 #
 # @author Trevor Vaughan <tvaughan@onyxpoint.com>
 #
 define svckill::ignore {
   include '::svckill::ignore::collector'
 
-  simpcat_fragment { "svckill_ignore+${name}.ignore": content => "${name}\n" }
+  ensure_resource('concat::fragment', "svckill_ignore_${name}", {
+    'target'  => $::svckill::ignore::collector::default_ignore_file,
+    'content' => $name
+  })
 }
