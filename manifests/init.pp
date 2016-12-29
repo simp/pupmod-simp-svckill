e6312aadae8440488c405988fea65aef0dd6e5e1
Dylan Cochran <dylan.cochran@onyxpoint.com>
2016-12-29 16:08:21 -0500

    SIMP-2342 Refactor svckill to use moduledata
    * Refactor svckill to use moduledata, and use
      hiera to pull the default ignore list. This
      allows us to use any fact with minimal effort
      in the service list.

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,6 +38,9 @@
 # @param ignore
 #   A list of services to never kill
 #
+# @param ignore_defaults
+#   An internal list of embedded services to never kill
+#
 # @param ignore_files
 #   A list of files that contain services to never kill, one per line
 #
@@ -52,21 +55,32 @@
 #   * If ``false``, it will only report on the number of services that it
 #     attempted to kill
 #
+# @param debug
+#   Add notify resources to the catalog based on the union of ignore and
+#   ignore_defaults. 
+#   
+#
+#
 # @author Trevor Vaughan <tvaughan@onyxpoint.com>
 #
 class svckill (
   Array[String]               $ignore       = [],
+  Array[String]               $ignore_defaults = [],
   Array[Stdlib::Absolutepath] $ignore_files = [],
-  Boolean                     $verbose      = true
+  Boolean                     $verbose      = true,
+  Boolean                     $debug        = false
 ){
   include '::svckill::ignore::collector'
-
+  $combined_ignore_list = $ignore + $ignore_defaults
+  if ($debug == true) {
+    $combined_ignore_list.each |String $servicename| {
+      notify { "svckill::ignore - entry ${servicename}": }
+    }
+  }
+  $flattened_ignore_files = flatten([$ignore_files, $::svckill::ignore::collector::default_ignore_file])
   svckill { 'svckill':
-    ignore      => $ignore,
-    ignorefiles => flatten([
-      $ignore_files,
-      $::svckill::ignore::collector::default_ignore_file
-    ]),
+    ignore      => $combined_ignore_list,
+    ignorefiles => $flattened_ignore_files,
     verbose     => $verbose,
     require     => Class['svckill::ignore::collector']
   }
