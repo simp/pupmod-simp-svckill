f2d6fa20f3cc03ffcfa03d584fcc9077b527e92e
Chris Tessmer <chris.tessmer@onyxpoint.com>
2017-02-22 02:54:53 -0500

    Update test to use actual nfs services
    Use `BEAKER_pre=svckill` to muck about within the tests.
    
    As far as I can see, svckill/service{}/systemctl already seems smart
    enough to treat symlinked services the same as their targets.  I
    couldn't reproduce the `nfs`/`nfs-server` behavior @jeefberkey mentioned
    yesterday afternoon.

--- spec/acceptance/suites/default/01_symlinked_services_spec.rb
+++ spec/acceptance/suites/default/01_symlinked_services_spec.rb
@@ -8,24 +8,29 @@ describe 'Not kill services which are symlinked to other services' do
     let(:manifest) {
       <<-EOF
       include 'svckill'
-      service { 'httpd': }
+      service { 'nfs': }
       EOF
     }
     # This issue only exists on systemd systems
     next if JSON.load(fact_on(host, 'os', { :json => nil }))['os']['release']['major'] == '6'
     context 'default parameters' do
       it 'should set up an essential service' do
-        on host, ('puppet resource package httpd ensure=latest')
-        on host, ('puppet resource service httpd ensure=running')
-        on host, ('curl localhost')
-      end
-      it 'should create a symlink to that service' do
-        on host, ('ln -s /usr/lib/systemd/system/httpd.service /usr/lib/systemd/system/apache.service')
+        # dnsmasq is sometimes still running and triggers svckill
+        on host, ('puppet resource service dnsmasq ensure=stopped')
+        on host, ('puppet resource package nfs ensure=latest')
+        on host, ('puppet resource service nfs ensure=running')
       end
       it 'should run puppet and not kill the application' do
         result = apply_manifest_on(host, manifest, :catch_failures => true).stdout
-        expect(result).to_not match(/stopped.*'apache/)
-        on host, ('curl localhost')
+        (require 'pry'; binding.pry) if ENV['BEAKER_pry'].chomp == 'svckill'
+
+        # svckill/service/systemctl already seems smart enough to treat
+        # symlinked services the same as their targets
+        expect(result).to_not match(/stopped.*'nfs-server/)
+        expect(result).to_not match(/stopped.*'nfs/)
+        ### # gssproxy is not symlinked to nfs.service, but will be stopped
+        ### # nfs.service -> auth-rpcgss-module.service -> gssproxy.service
+        ### expect(result).to_not match(/stopped.*'gssproxy/)
       end
 
     end
