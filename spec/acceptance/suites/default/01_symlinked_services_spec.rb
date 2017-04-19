ddda3392f6010bd012ffa47869e3e0bcaa46ee32
nmarkowski <nmarkowski@keywcorp.com>
2017-04-19 11:04:53 -0400

    (SIMP-52) Fix systemd alias check
    - In the service kill provider, not all services queried by 'Name'
      with systemctl would return a name.  That caused a break in symlink
      logic, rendering a majority of aliases to remain undetected.  Error
      detection has been added and the symlink logic has been re-worked.
    
    SIMP-52 #comment updated svckill
    SIMP-3081 #close

--- spec/acceptance/suites/default/01_symlinked_services_spec.rb
+++ spec/acceptance/suites/default/01_symlinked_services_spec.rb
@@ -12,28 +12,31 @@ describe 'not kill services which are symlinked to other services' do
       let(:manifest) {
         <<-EOF
         class { 'svckill':
-          verbose => true
+          verbose => true,
+          mode    => 'enforcing'
         }
         service { 'nfs-server': }
         EOF
       }
+
       it 'should set up an essential service' do
         # dnsmasq is sometimes still running and triggers svckill
         on host, 'puppet resource service dnsmasq ensure=stopped'
         on host, 'puppet resource package nfs ensure=latest'
         on host, 'puppet resource service nfs ensure=running'
       end
+
       it 'should run puppet and not kill the application' do
         result = apply_manifest_on(host, manifest, :catch_failures => true).stdout
         (require 'pry'; binding.pry) if ENV.fetch('BEAKER_pry','').chomp == 'svckill'
 
-        # svckill/service/systemctl already seems smart enough to treat
-        # symlinked services the same as their targets
+        # In el7, nfs.service is an alias of nfs-server.service and should not be
+        # stopped or disabled
         expect(result).to_not match(/stopped.*'nfs-server/)
-        expect(result).to_not match(/stopped.*'nfs/)
-        ### # gssproxy is not symlinked to nfs.service, but will be stopped
-        ### # nfs.service -> auth-rpcgss-module.service -> gssproxy.service
-        ### expect(result).to_not match(/stopped.*'gssproxy/)
+        expect(result).to_not match(/stopped.*'nfs.service'/)
+        expect(result).to_not match(/disabled.*'nfs.service'/)
+        # Without kerberos, this should not be running
+        expect(result).to match(/stopped.*'gssproxy.service'/)
       end
     end
 
@@ -41,25 +44,25 @@ describe 'not kill services which are symlinked to other services' do
       let(:manifest) {
         <<-EOF
         class { 'svckill':
-          verbose => true
+          verbose => true,
+          mode    => 'enforcing'
         }
         service { 'gdm': }
         EOF
       }
+
       it 'should set up an essential service' do
         # dnsmasq is sometimes still running and triggers svckill
         on host, 'puppet resource service dnsmasq ensure=stopped'
-
         on host, 'yum install -y @x11 gdm gnome-shell gnome-session-xsession'
         on host, 'systemctl enable gdm.service --now'
-
         on host, 'systemctl set-default graphical.target'
         on host, 'systemctl isolate graphical.target'
       end
+
       it 'should run puppet and not kill the application' do
         result = apply_manifest_on(host, manifest, :catch_failures => true).stdout
         (require 'pry'; binding.pry) if ENV.fetch('BEAKER_pry','').chomp == 'svckill'
-
         expect(result).to_not match(/stopped.*'gdm/)
         expect(result).to_not match(/stopped.*'display-manager/)
       end
