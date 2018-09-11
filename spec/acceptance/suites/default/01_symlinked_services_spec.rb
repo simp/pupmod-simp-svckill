7a70cd83dac5ddfeb8425bd9154931f81d45e576
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2018-09-11 10:29:02 -0400

    (SIMP-2791) Update to Hiera 5 (#44)
    - Update to Hiera 5
    - Fix bug in .gitlab-ci.yml
    - Add logic to skip GUI tests in OEL, as it requires manual
      input at the GUI in order to run
    
    SIMP-2791 #comment pupmod-simp-svckill
    SIMP-5335 #comment pupmod-simp-svckill

--- spec/acceptance/suites/default/01_symlinked_services_spec.rb
+++ spec/acceptance/suites/default/01_symlinked_services_spec.rb
@@ -52,20 +52,30 @@ describe 'not kill services which are symlinked to other services' do
       }
 
       it 'should set up an essential service' do
-        # dnsmasq is sometimes still running and triggers svckill
-        on(host, 'puppet resource service dnsmasq ensure=stopped')
-        on(host, 'yum install -y @x11 gdm gnome-shell gnome-session-xsession')
-        host.reboot
-        on(host, 'systemctl enable gdm.service --now')
-        on(host, 'systemctl set-default graphical.target')
-        on(host, 'systemctl isolate graphical.target')
+        facts = JSON.load(on(host, 'puppet facts').stdout)
+        if facts['values']['os']['name'] == 'OracleLinux'
+          skip("FIXME: Test can't be run on OracleLinux because it requires physical input at console")
+        else
+          # dnsmasq is sometimes still running and triggers svckill
+          on(host, 'puppet resource service dnsmasq ensure=stopped')
+          on(host, 'yum install -y @x11 gdm gnome-shell gnome-session-xsession')
+          host.reboot
+          on(host, 'systemctl enable gdm.service --now')
+          on(host, 'systemctl set-default graphical.target')
+          on(host, 'systemctl isolate graphical.target')
+        end
       end
 
       it 'should run puppet and not kill the application' do
-        result = apply_manifest_on(host, manifest, :catch_failures => true).stdout
-        (require 'pry'; binding.pry) if ENV.fetch('BEAKER_pry','').chomp == 'svckill'
-        expect(result).to_not match(/stopped.*'gdm/)
-        expect(result).to_not match(/stopped.*'display-manager/)
+        facts = JSON.load(on(host, 'puppet facts').stdout)
+        if facts['values']['os']['name'] == 'OracleLinux'
+          skip("FIXME: Test can't be run on OracleLinux because it requires physical input at console")
+        else
+          result = apply_manifest_on(host, manifest, :catch_failures => true).stdout
+          (require 'pry'; binding.pry) if ENV.fetch('BEAKER_pry','').chomp == 'svckill'
+          expect(result).to_not match(/stopped.*'gdm/)
+          expect(result).to_not match(/stopped.*'display-manager/)
+        end
       end
 
     end
